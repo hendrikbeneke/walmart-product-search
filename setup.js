99b5540a3075b5e3d1604025c4ec5a579db9f878
Hendrik Beneke <hendrik.beneke@gmail.com>
2018-10-05 18:08:51 +0200

    extracted setup es template

--- setup.js
+++ setup.js
@@ -1,17 +1,5 @@
 const log = require('./lib/logger');
-const esClient = require('./lib/elasticsearch');
-const esTemplate = require('./esTemplate/products');
-
-const setupEsTemplate = async () => {
-  log.info('Setting up ES templates');
-  log.debug(`Template: ${JSON.stringify(esTemplate)}`);
-
-  await esClient.indices.putTemplate({
-    name: 'products_template',
-    body: esTemplate
-  });
-  log.info('Done setting up ES templates');
-};
+const setupEsTemplate = require('./lib/setup-es-template');
 
 const setup = async () => {
   log.info('Starting setup');
